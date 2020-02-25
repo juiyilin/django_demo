e9a9d0a25846e5c2fd5a748161a4a4ef4fb44842
join <join830925@gmail.com>
2020-02-25 21:48:15 +0800

    heroku setting

--- final/final/wsgi.py
+++ final/final/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final.settings')
-
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+application = Cling(get_wsgi_application())
