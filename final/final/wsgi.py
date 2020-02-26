0675e62cfe938febcf1001a3af1e93283ee99ce3
join <join830925@gmail.com>
2020-02-26 21:24:38 +0800

    revise wsgi error

--- final/final/wsgi.py
+++ final/final/wsgi.py
@@ -11,5 +11,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "final.settings")
 application = Cling(get_wsgi_application())
