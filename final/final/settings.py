dffe03027b65b1a947722c139c6cf18f29ab9392
join <join830925@gmail.com>
2020-02-28 21:25:20 +0800

    modified:   Procfile

--- final/final/settings.py
+++ final/final/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '13n^zjaf$@%6sn=($dlg064vr6uvr84tnv14$zm@_802s86^0='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -119,11 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 MEDIA_ROOT = 'static/image/'  # 存放admin上傳圖片的路徑
 MEDIA_URL = '/image/'
 GOOGLE_RECAPTCHA_SECRET_KEY = '6LdzBtYUAAAAAOkYvtNVLrUIAL14-JLsMfiLke6D'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
