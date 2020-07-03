d5e300db7fd45adbb0ba86a5c66c062fbef6fe5e
join <join830925@gmail.com>
2020-07-03 23:22:20 +0800

    scrollbar iphone

--- final/urls.py
+++ final/urls.py
@@ -39,4 +39,6 @@ urlpatterns = [
     path('autobiography/', views.autobiography, name='autobiography'),
     path('portfolio/', include(portfolio_url)),
     path("captcha/", include("captcha.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
